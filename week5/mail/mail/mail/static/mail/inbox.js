4871c458da965b62262ac168890faa8148608fbc
tuan220199 <phamtuan.220199@gamil.com>
2022-12-15 14:40:15 +0700

    Complete the send_email requirement

--- week5/mail/mail/mail/static/mail/inbox.js
+++ week5/mail/mail/mail/static/mail/inbox.js
@@ -6,6 +6,9 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  //Send email 
+  document.querySelector('#compose-form').addEventListener('submit', send_email);
+
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -30,4 +33,61 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  //get a request to email/mailbox to request email with FETCH 
+  fetch('/emails/inbox')
+  .then(response => response.json())
+  .then(emails => {
+    // Print emails
+    //console.log(emails);
+
+    // Extract the items of email objects in an array of emails
+    emails.forEach(function(email){       
+       
+      // Extract each key-value pair in email object
+      const sender = email["sender"];
+      const recipients = email["recipients"];
+      const subject = email["subject"];
+      const body = email["body"];
+      const timestamp = email["timestamp"];
+      const read = email["read"];
+      const archived = email["archived"]; 
+
+      // Create the new div element
+      const element = document.createElement('div');
+      element.innerHTML = `${sender}   ${body}`;
+      document.querySelector('#emails-view').append(element);
+    }); 
+  });
+}
+
+function send_email(event)
+{
+  // Prevent the event to be submitted and show the result in console
+  event.preventDefault();
+  //console.log("HI");
+  // Retrieve data from HTML file to Java Script file
+  recipient = document.querySelector('#compose-recipients');
+  subject = document.querySelector('#compose-subject');
+  body = document.querySelector('#compose-body');
+
+  // Disable submit button by default:
+  //submit.disabled = true;
+
+  // Check the email susscessful or not 
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: recipient.value,
+        subject: subject.value,
+        body: body.value
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+      // Print result
+      console.log(result);
+      load_mailbox('sent');
+  });
+
 }
