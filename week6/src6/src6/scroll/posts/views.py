3579103f9538f993195b60bf478c788e9e31df2b
tuan220199 <phamtuan.220199@gamil.com>
2022-12-23 18:07:38 +0700

    Fix some files to review

--- week6/src6/src6/scroll/posts/views.py
+++ week6/src6/src6/scroll/posts/views.py
@@ -8,20 +8,16 @@ def index(request):
     return render(request, "posts/index.html")
 
 def posts(request):
-
-    # Get start and end points
     start = int(request.GET.get("start") or 0)
-    end = int(request.GET.get("end") or (start + 9))
+    end = int(request.GET.get("end") or (start+20))
 
-    # Generate list of posts
-    data = []
-    for i in range(start, end + 1):
+    data =[]
+    for i in range(start, end+1):
         data.append(f"Post #{i}")
 
-    # Artificially delay speed of response
     time.sleep(1)
 
-    # Return list of posts
-    return JsonResponse({
-        "posts": data
-    })
+    return JsonResponse(
+        {"posts": data}
+    )
+    
